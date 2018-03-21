681070cb4bd8c31a2f58a882b9d2cec4d05f4f6f
JoshuaMills <Jbmills291@gmail.com>
2018-03-21 18:49:55 -0400

    interface added to get raw data for callback function.

---
+++ app/src/main/java/com/example/mills/b/joshua/flickerphotoapp/Photo.java
@@ -0,0 +1,15 @@
+package com.example.mills.b.joshua.flickerphotoapp;
+
+/**
+ * Created by Inferno on 3/21/2018.
+ */
+
+class Photo {
+    private String title;
+    private String author;
+    private String authorId;
+    private String link;
+    private String tags;
+    private String image;
+
+}
