5d5de3172c9e0f128871e809e9c1103289f618bc
JoshuaMills <Jbmills291@gmail.com>
2018-04-01 02:07:29 -0400

    cleanup

--- app/src/main/java/com/example/mills/b/joshua/flickerphotoapp/PhotoDetailActivity.java
+++ app/src/main/java/com/example/mills/b/joshua/flickerphotoapp/PhotoDetailActivity.java
@@ -1,6 +1,7 @@
 package com.example.mills.b.joshua.flickerphotoapp;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,6 +12,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
+
+import java.text.Format;
 
 public class PhotoDetailActivity extends BaseActivity {
 
@@ -23,14 +26,15 @@ public class PhotoDetailActivity extends BaseActivity {
         Intent intent = getIntent();
         Photo photo = (Photo) intent.getSerializableExtra(PHOTO_TRANSFER);
         if(photo!=null){
+            Resources resources = getResources();
             TextView photoTitle = findViewById(R.id.photo_title);
-            photoTitle.setText("Title: "+photo.getTitle());
+            photoTitle.setText(String.format(resources.getString(R.string.photo_title_text),photo.getTitle()));
 
             TextView photoTags = findViewById(R.id.photo_tags);
-            photoTags.setText("Tags: " + photo.getTags());
+            photoTags.setText(String.format(resources.getString(R.string.photo_tags_text),photo.getTags()));
 
             TextView photoAurthor = findViewById(R.id.photo_author);
-            photoAurthor.setText("Aurthor: "+photo.getAuthor());
+            photoAurthor.setText(String.format(resources.getString(R.string.photo_author_text),photo.getAuthor()));
 
             ImageView imageView = findViewById(R.id.photo_image);
 
